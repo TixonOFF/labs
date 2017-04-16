59357896472f749672e7d974c037076a4728609d
PandekoAndrew <pandeko.andrew@gmail.com>
2017-04-16 21:01:36 +0300

    Add files via upload

---
+++ Hash/CloseItem.java
@@ -0,0 +1,45 @@
+package by.vsu.Hash;
+
+public class CloseItem<V>
+{
+	private int key;
+	private V value;
+	
+	public CloseItem(int key, V value)
+	{
+		this.key = key;
+		this.value = value;
+	}
+
+	public int getKey() 
+	{
+		return key;
+	}
+
+	public void setKey(int key) 
+	{
+		this.key = key;
+	}
+
+	public V getValue() 
+	{
+		return value;
+	}
+
+	public void setValue(V value) 
+	{
+		this.value = value;
+	}
+	
+	@Override
+	public String toString() 
+	{
+		StringBuilder itemBuilder = new StringBuilder();
+		
+		itemBuilder.append("Ключ: ");
+		itemBuilder.append(key);
+		itemBuilder.append("   Значение: ");
+		itemBuilder.append(value);
+		return itemBuilder.toString();
+		}
+}
